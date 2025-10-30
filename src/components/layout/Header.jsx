7d6e395abb5a3443c649ff4d4b7cda16e030d72b
maryamessam22-pixel <maryamessam22@gmail.com>
2025-10-30 21:14:26 +0300

    Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,6 +7,12 @@ const Header = () => {
     return (
 <>
 
+   <div className='txt'>
+      <h1 className='txth1'>CIB</h1>
+             <h2>Delivering value to our clients, our<br></br>
+                 community and our shareholders</h2>
+   </div>
+
        <img src={hero} alt="hero" className="hero" />
 
 
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
  <>  
       <Nav />
       <Header />
+       
+      
     </>
 );
 }
