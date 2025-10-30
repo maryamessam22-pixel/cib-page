46c91a132824f56a24ed2a3fb7a1edc48f2df293
maryamessam22-pixel <maryamessam22@gmail.com>
2025-10-30 20:38:07 +0300

    hero

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import Header from '../components/layout/Header';
+import hero from '../../Assets/hero.png';
+import "./Header.css";
+
+const Header = () => {
+    return (
+<>
+
+       <img src={hero} alt="hero" className="hero" />
+
+
+  
+</>
+        
+      );
+}
+ 
+export default Header;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import "./Home.css"
 import Nav from '../components/layout/Nav';
+import Header from '../components/layout/Header';
 
 
 const Home = () => {
     return ( 
  <>  
-
       <Nav />
-
-    
+      <Header />
     </>
 );
 }
