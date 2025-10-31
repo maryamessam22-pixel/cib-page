2c97b57133eee71a78df5b6ba4eb3108b0a0c40a
maryamessam22-pixel <maryamessam22@gmail.com>
2025-10-31 12:56:08 +0200

    SmallMenuTap

--- src/components/common/SmallMenuTap.jsx
+++ src/components/common/SmallMenuTap.jsx
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 import './SmallMenuTap.css'
 const SmallMenuTap = (props) => {
     return ( 
-<>
-
+        <>
        <p className="small-container"> {props.title}</p>
-
-  
-         
-
 </>
 
      );
